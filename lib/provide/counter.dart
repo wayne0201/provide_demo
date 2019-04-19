f851b1d8d796fecf9ac2d9b50e579b38be18cc5e
wayne0201 <614418910@qq.com>
2019-04-19 18:15:38 +0800

    提交

--- lib/provide/counter.dart
+++ lib/provide/counter.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 
-class Counter with ChangeNotifier{
+class Counter with ChangeNotifier {
   int _value;
   /* value 只读，只能通过暴露的方法改变 */
+
   int get value => _value;
 
   Counter(this._value);
 
   add() {
-    _value ++;
+    _value++;
     notifyListeners();
   }
 
   minus() {
-    _value --;
+    _value--;
     notifyListeners();
   }
-
 }
